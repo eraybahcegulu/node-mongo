897fe8ef38595c5071d2f2ce6e704a651f03510a
Eray BAHÇEGÜLÜ <eraybahcegulu@gmail.com>
2023-10-01 00:42:10 +0300

    giriş yapan kullanıcının diğer kullanıcılara özel mesaj gönderebilmesi eklendi

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = 3000;
+app.use(bodyParser.json()); 
 
 app.set('view engine', 'ejs');
 
@@ -18,6 +19,7 @@ db.once('open', function () {
 
 const messageSchema = new mongoose.Schema({
     content: String,
+    sender : String,
     timestamp: { type: Date, default: Date.now },
 });
 
@@ -36,29 +38,26 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model('User', userSchema);
 
-const addUserMessage = async (email, messageContent) => {
+const addUserMessage = async (email, messageContent, messageSender) => {
     try {
         const user = await User.findOne({ email });
         if (!user) {
             console.log('Kullanıcı bulunamadı.');
             return;
         }
-
-        user.messages.push({ content: messageContent });
+        user.messages.push({ content: messageContent, sender: messageSender});
         await user.save();
-
         console.log('Mesaj başarıyla eklendi.');
     } catch (error) {
         console.error('Hata:', error.message);
     }
 };
 
-const email = '1@1';
-addUserMessage(email, 'test mesajı');
-
 app.use(express.static(path.join(__dirname, 'public/html')));
 app.use(express.static(path.join(__dirname, 'public/css')));
+
 app.set('views', path.join(__dirname, 'public/views'));
+app.use(express.static(path.join(__dirname, 'public/views')));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req,res) =>{
@@ -69,6 +68,9 @@ app.get('/register', (req,res) =>{
     res.sendFile(path.join(__dirname,'public/html', 'register.html'));
 })
 
+app.get('/home', (req,res) =>{
+    res.sendFile(path.join(__dirname,'public/views', 'home.ejs'));
+})
 
 app.post('/register', async (req, res) => {
     try {
@@ -97,7 +99,7 @@ app.post('/register', async (req, res) => {
     }
 });
 
-app.post('/', async (req, res) => {
+app.post('/home', async (req, res) => {
     try {
         const { email, password } = req.body;
         const user = await User.findOne({ email: email, password: password });
@@ -117,6 +119,18 @@ app.post('/', async (req, res) => {
     }
 });
 
+app.post('/sendMessage', (req, res) => {
+    const selectedEmail = req.body.selectedEmail;
+    const selectedMessage = req.body.selectedMessage;
+    const userLoggedInEmail = req.body.userLoggedInEmail;
+
+    addUserMessage(selectedEmail , selectedMessage, userLoggedInEmail);
+
+    const successMessage = 'Mesaj gönderildi';
+    res.json({ success: successMessage });
+});
+
 app.listen(PORT, () => {
     console.log(`Server çalışıyor: http://localhost:${PORT}`);
   });
+
